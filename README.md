dcf6ccc212056dc650eafdb9d64f0ffac88c42ac
iibrat <test@runoob.com>
2023-04-19 19:52:42 +0800

    更新提示

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 <p>留意代码中的注释</p>
 
 ## 运行项目
+- `yarn`
+- `yarn dev`
+
+## 浏览器按F12打开控制台，数据打印在控制台
 
 
 This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
--- src/client/components/HelloWorld.vue
+++ src/client/components/HelloWorld.vue
@@ -3,23 +3,42 @@
 export default {
   data() {
     return {
+      // resData: ''
     }
   },
   methods: {
     async onCheck() {
 
-      // 用fetch向服务器发送请求
+      // 本地测试 '/hello' = 'http://localhost:3000/hello'
+      // 用 fetch 发起请求
       let res = await fetch('/hello')
-      //用res.body.getReader()获取数据流内容
-      //pipeThrough(new TextDecoderStream()) 这个是用来转换数据格式
+
+      // new TextDecoderStream() 将二进制数据转换成字符串数据
+      // pipeThrough() 将body响应体转换成 ReadableStreamDefaultReader 对象
+      // pipeThrough() 中的参数 new TextDecoderStream() 这是一个构造函数，创建一个类，
+      // 这个类可以将 body的二进制数据转换成字符串数据
+      // getReader() 获取 ReadableStreamDefaultReader 对象，这个对象可以用来逐块读取服务端逐块
+      // 发送过来的数据。
       const reader = res.body.pipeThrough(new TextDecoderStream()).getReader()
+
+      // 循环读取服务端发送过来的数据
       while (true) {
+
+        // 获取 reader.read() 中的属性 done 和 value
+        // done：done 默认为 false, 服务端数据发送完毕后会调用 res.end() 此时 done==false
+        // 用来表示数据发送完毕，下面 if(done) 就是用来验证数据是否发送完毕
+        // value：服务端发送过来的数据
         const { done, value } = await reader.read()
+
+        // 如果数据发送完毕，break 跳出循环
         if (done) {
           console.log(done)
           break
         }
+
+        // 输出服务端发送来的数据。如果上面 done==true 这里的代码不会执行，因为 break 跳出了循环。
         console.log(`这是服务端返回的数据：${value}`)
+        // this.resData += resData
       }
 
     }
@@ -30,7 +49,8 @@ export default {
 </script>
 
 <template>
-  <el-button @click="onCheck">提交</el-button>
+  <el-button @click="onCheck">Fetch请求</el-button>
+  <!-- <div>{{ resData }}</div> -->
 </template>
 
 <style scoped></style>
